a2da9276a22cf8d354440fd5d0006cbcf08edfc1
bajpaidhruv <dhruvbajpai97@gmail.com>
2025-03-04 23:07:51 +0530

    Final White Screen Fix

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Add this line
+  base: './', // This is correct
+  build: {
+    outDir: 'dist', // This is optional but recommended
+  }
 })
