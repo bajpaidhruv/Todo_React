8a0062106d8afc77955094e19826efeccbdd1d7f
bajpaidhruv <dhruvbajpai97@gmail.com>
2025-03-04 23:16:19 +0530

    finall h yrr

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // This is correct
-  build: {
-    outDir: 'dist', // This is optional but recommended
-  }
+  base: '/', // Change this from './' to '/'
 })
